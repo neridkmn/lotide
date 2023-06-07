4cddfd9a51456692b27fa03b8a97052ba8056f09
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-07 19:59:46 -0400

    Created new file test/middleTest.js / required assertArraysEqual & middle functions to test. Modified middle.js with module.exports

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // check if both arrays have the same length.
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) { // loop over each element & check equality. return false if there is a mismatch
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true; // no mismatches were found so return true.
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅Assertion passed: Two arrays are equal.`);
-  } else {
-    console.log(`🛑Assertion failed: Two arrays are not equal.`);
-  }
-};
-
 const middle = function(inputArray) {
   if (inputArray.length <= 2) {
     return [];
@@ -36,10 +14,4 @@ const middle = function(inputArray) {
 };
 
 
-
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
