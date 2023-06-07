5972168a7303804f6698283907ced4e0e4c3ea01
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-07 19:47:51 -0400

    Refactored with module.exports & require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // check if both arrays have the same length.
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) { // loop over each element & check equality. return false if there is a mismatch
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true; // no mismatches were found so return true.
-};
+const eqArrays = require('./eqArrays'); //same directory
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
@@ -20,5 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-console.log(assertArraysEqual([5, 3], [3, 2]));
-console.log(assertArraysEqual([7, 7], [7, 7]));
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+
+
+const assertArraysEqual = require('../assertArraysEqual');
+
+console.log(assertArraysEqual([5, 3], [3, 2]));
+console.log(assertArraysEqual([7, 7], [7, 7]));
