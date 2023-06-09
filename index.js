1d1239b16886061818f09114cc7d10a018c7699b
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-09 15:59:31 -0400

    index.js for functions as objects

---
+++ index.js
@@ -0,0 +1,37 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const eqArrays = require('./eqArrays');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  without,
+  takeUntil,
+  eqArrays,
+  map,
+  letterPositions,
+  flatten,
+  findKeyByValue,
+  findKey,
+  eqObjects,
+  countOnly,
+  countLetters,
+  assertObjectsEqual
+};
