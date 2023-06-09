3ba29ceea51c0a685ed3460a8ff9eb26c0f0e146
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-09 15:31:04 -0400

    Refactored w/ module.exports & require, created a new test file

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,10 @@
-const assertEqual = function(actual, expected) { // can only compare primitive values
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
-  for (const item of allItems) {  // In order to know how many Jason's are in our list, we need to scan the entire list. Let's start with a simple loop and make sure it 'works.
+  for (const item of allItems) {  // In order to know how many Jason's are in our list, we need to scan the entire list. Let's start with a simple loop and make sure it works.
     // inside the loop,
     // increment the counter for each item:
     //   set a property with that string key to:
@@ -29,21 +22,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
